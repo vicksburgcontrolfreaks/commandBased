810bbfd6ab6c48d9cf0acfdb9769ec291226848c
TakeHomePC <vicksburgcontrolfreaks@gmail.com>
2022-01-26 17:07:18 -0500

    Collector and PID

---
+++ src/main/java/frc/robot/commands/DriveDistance.java
@@ -0,0 +1,72 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.Constants.DriveConstants;
+import frc.robot.subsystems.MechTrain;
+
+public class DriveDistance extends CommandBase {
+  private final MechTrain m_drive;
+  private final Double m_dist;
+  private final Double m_speed;
+  double frontLeftF;
+  double frontRightF;
+  double backLeftF;
+  double backRightF;
+  /** Creates a new DriveDistance. */
+  public DriveDistance(MechTrain subsystem, double x, double s) {
+    m_drive = subsystem;
+    m_dist = x;
+    m_speed = s;
+    addRequirements(m_drive);
+    // Use addRequirements() here to declare subsystem dependencies.
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+    double ticDist = m_dist*DriveConstants.ticksPerInch;
+    double frontLeftF = m_drive.frontLeftEncoderV() + ticDist;
+    double frontRightF = m_drive.frontRightEncoderV() - ticDist;
+    double backLeftF = m_drive.backLeftEncoderV() + ticDist;
+    double backRightF = m_drive.backRightEncoderV() - ticDist;
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    m_drive.driveFrontLeft(frontLeftF, m_speed, 0);
+    m_drive.driveFrontRight(frontRightF, m_speed, 0);
+    m_drive.driveBackLeft(backLeftF, m_speed, 0);
+    m_drive.driveBackRight(backRightF, m_speed, 0);
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {}
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    double frontLeftEValue = m_drive.frontLeftEncoderV();
+    double frontRightEValue = m_drive.frontLeftEncoderV();
+    double backLeftEValue = m_drive.frontLeftEncoderV();
+    double backRightEValue = m_drive.frontLeftEncoderV();
+    double drive_encoderError = DriveConstants.drive_encoderError;
+    boolean drivePostionReached = true;
+    if (Math.abs(frontLeftF - frontLeftEValue) > drive_encoderError)
+    drivePostionReached = false;
+    if (Math.abs(frontRightF - frontRightEValue) > drive_encoderError)
+    drivePostionReached = false;
+    if (Math.abs(backLeftF - backLeftEValue) > drive_encoderError)
+    drivePostionReached = false;
+    if (Math.abs(backRightF - backRightEValue) > drive_encoderError)
+    drivePostionReached = false;
+    if(m_drive.avgV() > .5)
+    drivePostionReached = false;
+    return drivePostionReached;
+  }
+}
--- src/main/java/frc/robot/subsystems/MechTrain.java
+++ src/main/java/frc/robot/subsystems/MechTrain.java
@@ -5,23 +5,118 @@
 package frc.robot.subsystems;
 
 import com.revrobotics.CANSparkMax;
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.SparkMaxPIDController;
+import com.revrobotics.CANSparkMax.ControlType;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 import edu.wpi.first.wpilibj.drive.MecanumDrive;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.DriveConstants;
 
 public class MechTrain extends SubsystemBase {
   private final CANSparkMax frontLeft = new CANSparkMax(1, MotorType.kBrushless);
   private final CANSparkMax frontRight = new CANSparkMax(2, MotorType.kBrushless);
   private final CANSparkMax backLeft = new CANSparkMax(3, MotorType.kBrushless);
   private final CANSparkMax backRight = new CANSparkMax(4, MotorType.kBrushless);
+  private final SparkMaxPIDController frontLeftP = frontLeft.getPIDController();
+  private final SparkMaxPIDController frontRightP = frontRight.getPIDController();
+  private final SparkMaxPIDController backLeftP = backLeft.getPIDController();
+  private final SparkMaxPIDController backRightP = backRight.getPIDController();
+  private final RelativeEncoder frontLeftE = frontLeft.getEncoder();
+  private final RelativeEncoder frontRightE = frontRight.getEncoder();
+  private final RelativeEncoder backLeftE = backLeft.getEncoder();
+  private final RelativeEncoder backRightE = backRight.getEncoder();
 
   private final MecanumDrive mechDrive = new MecanumDrive(frontLeft, backLeft, frontRight, backRight);
   /** Creates a new MechTrain. */
   public void mecanumDrive(double x, double y){
     mechDrive.driveCartesian(y, x, 0);
   }
+
   public void quickTurn(double rot){
     mechDrive.driveCartesian(0, 0, rot);
+  }
+
+  public void setFrontLeftPids(int slot, double kMaxOutput, double kMinOutput){
+    //establishes the PID values for each of the drive motors
+    frontLeftP.setP(DriveConstants.drive_kP, slot);
+    frontLeftP.setI(DriveConstants.drive_kI, slot);
+    frontLeftP.setD(DriveConstants.drive_kD, slot);
+    frontLeftP.setIZone(DriveConstants.drive_kIz, slot);
+    frontLeftP.setFF(DriveConstants.drive_kFF, slot);
+    frontLeftP.setSmartMotionAllowedClosedLoopError(DriveConstants.drive_encoderError, slot);
+    frontLeftP.setOutputRange(kMinOutput, kMaxOutput, slot);
+  }
+
+  public void setFrontRightPids(int slot, double kMaxOutput, double kMinOutput){  
+    frontRightP.setP(DriveConstants.drive_kP, slot);
+    frontRightP.setI(DriveConstants.drive_kI, slot);
+    frontRightP.setD(DriveConstants.drive_kD, slot);
+    frontRightP.setIZone(DriveConstants.drive_kIz, slot);
+    frontRightP.setFF(DriveConstants.drive_kFF, slot);
+    frontRightP.setSmartMotionAllowedClosedLoopError(DriveConstants.drive_encoderError, slot);
+    frontRightP.setOutputRange(kMinOutput, kMaxOutput, slot);
+  }
+
+  public void setBackLeftPids(int slot, double kMaxOutput, double kMinOutput){
+    backLeftP.setP(DriveConstants.drive_kP, slot);
+    backLeftP.setI(DriveConstants.drive_kI, slot);
+    backLeftP.setD(DriveConstants.drive_kD, slot);
+    backLeftP.setIZone(DriveConstants.drive_kIz, slot);
+    backLeftP.setFF(DriveConstants.drive_kFF, slot);
+    backLeftP.setSmartMotionAllowedClosedLoopError(DriveConstants.drive_encoderError, slot);
+    backLeftP.setOutputRange(kMinOutput, kMaxOutput, slot);
+  }
+
+  public void setBackRightPids(int slot, double kMaxOutput, double kMinOutput){
+    backRightP.setP(DriveConstants.drive_kP, slot);
+    backRightP.setI(DriveConstants.drive_kI, slot);
+    backRightP.setD(DriveConstants.drive_kD, slot);
+    backRightP.setIZone(DriveConstants.drive_kIz, slot);
+    backRightP.setFF(DriveConstants.drive_kFF, slot);
+    backRightP.setSmartMotionAllowedClosedLoopError(DriveConstants.drive_encoderError, slot);
+    backRightP.setOutputRange(kMinOutput, kMaxOutput, slot);
+  }
+
+  public void driveFrontLeft(double distance, double speed, int slot){
+    setFrontLeftPids(0, speed, -speed);
+    frontLeftP.setReference(distance, ControlType.kPosition, 0);
+  }
+  
+  public void driveFrontRight(double distance, double speed, int slot){
+    setFrontRightPids(0, speed, -speed);
+    frontRightP.setReference(distance, ControlType.kPosition, 0);
+  }
+  
+  public void driveBackLeft(double distance, double speed, int slot){
+    setBackLeftPids(0, speed, -speed);
+    backLeftP.setReference(distance, ControlType.kPosition, 0);
+  }
+  
+  public void driveBackRight(double distance, double speed, int slot){
+    setBackRightPids(0, speed, -speed);
+    backRightP.setReference(distance, ControlType.kPosition, 0);
+  }
+
+  public double frontLeftEncoderV(){
+    return frontLeftE.getPosition();
+  }
+
+  public double frontRightEncoderV(){
+    return frontRightE.getPosition();
+  }
+
+  public double backLeftEncoderV(){
+    return backLeftE.getPosition();
+  }
+
+  public double backRightEncoderV(){
+    return backRightE.getPosition();
+  }
+
+  public double avgV(){
+    double avg = (Math.abs(frontLeftE.getVelocity()) + Math.abs(frontRightE.getVelocity()) + Math.abs(backLeftE.getVelocity()) + Math.abs(backRightE.getVelocity()))/4;
+    return avg;
   }
 }
